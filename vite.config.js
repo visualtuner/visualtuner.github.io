76f17b693fa5d7cb5894d0f038086e05770c7009
SeokGoo Lee <visualtuner@gmail.com>
2025-04-24 17:29:23 +0900

    docs 폴더로 바꿈

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     base: '/SmartGuide/',
+    build: {
+        outDir: 'docs'
+    },
     plugins: [react()],
 })
