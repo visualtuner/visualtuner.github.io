3aff08ee085595040992eab9c7e6e6bf9e866500
SeokGoo Lee <visualtuner@gmail.com>
2025-04-25 08:45:15 +0900

    기본세팅-폰트,아이콘

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from "react-router-dom";
 
 function Home() {
-  return <h2>Home Page</h2>;
+	return <h2>Home Page</h2>;
 }
 
 function About() {
-  return <h2>About Page</h2>;
+	return <h2>About Page</h2>;
 }
 
 export default function App() {
-  return (
-    <>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<nav>
+				<Link to="/">Home</Link> | <Link to="/about">About</Link>
+			</nav>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</>
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import './assets/styles/global.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/SmartGuide/">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter basename="/SmartGuide/">
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
