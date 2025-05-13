1385a2d3c0a23afc28738dc1bc12025ad5878a13
SeokGoo Lee <visualtuner@gmail.com>
2025-05-13 13:56:54 +0900

    기본세팅 중

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,6 @@
-import { useNavigate } from 'react-router-dom';
 import Header from "../components/Header";
 
 export default function Home() {
-    const navigate = useNavigate();
-
 	return (
 		<>
 			<Header >
